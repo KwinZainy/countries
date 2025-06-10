cc2d9d16668c3bd2d9913918b608311accc1cbf4
KwinZainy <gbadamosiblessing651@gmail.com>
2025-06-10 21:42:03 +0100

    the region

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+
 let container = document.getElementById("container")
 let searchBox = document.getElementById("searchBox")
 let searchBtn = document.getElementById("searchBtn")
+let regionBar = document.getElementById("regionBar")
+let regionSlot = document.getElementById("regionSlot")
 
-fetch("https://restcountries.com/v3.1/all")
+fetch("https://restcountries.com/v3.1/all?fields=name,flags,countries,population,region,currencies")
 .then(response => response.json())
 .then(data =>{
     allCountries = data
@@ -11,8 +14,14 @@ fetch("https://restcountries.com/v3.1/all")
     
 })
 
+
 function showCountries(countries) {
+    
+
         countries.forEach(element => {
+    let currencyKey = element.currencies ? Object.keys(element.currencies)[0] : 'NULL';
+    let currencyName = currencyKey ? element.currencies[currencyKey].name : "N/A";
+    let currencySymbol = currencyKey ? element.currencies[currencyKey].symbol : "N/A";
         container.innerHTML +=`
         <div class="box">
             <div class="image">
@@ -21,8 +30,8 @@ function showCountries(countries) {
             <div class="information">
                 <p>Country: ${element.name.common}</p>
                 <p>Population: ${element.population}</p>
-                <p>Currency: ${element.currencies[Object.keys(element.currencies)[0]].name}</p>
-                <p>Symbol: ${element.currencies[Object.keys(element.currencies)[0]].symbol}</p>
+                <p>Currency: ${currencyName}</p>
+                <p>Symbol: ${currencySymbol}</p>
             </div>
         </div>`
     });
@@ -49,15 +58,45 @@ function searched() {
             <div class="information">
                 <p>Country: ${element.name.common}</p>
                 <p>Population: ${element.population}</p>
-                <p>Currency: ${element.currencies[Object.keys(element.currencies)[0]].name}</p>
-                <p>Symbol: ${element.currencies[Object.keys(element.currencies)[0]].symbol}</p>
+
             </div>
         </div>`
     });
 }
 
+
+function region() {
+    container.innerHTML = ""
+    let getRegion = regionBar.value
+    fetch(`https://restcountries.com/v3.1/region/${getRegion}`)
+    .then(response => response.json())
+    .then(data => {
+        
+        regionSlot.innerHTML = `<h1>THE ${getRegion.toUpperCase()} COUNTRIES`
+
+        data.forEach(element =>{
+            let currencyKey = element.currencies ? Object.keys(element.currencies)[0] : null;
+            let currencyName = currencyKey ? element.currencies[currencyKey].name : "N/A";
+            let currencySymbol = currencyKey ? element.currencies[currencyKey].symbol : "N/A";
+            container.innerHTML +=`
+            <div class="box">
+            <div class="image">
+                <img src="${element.flags.png}" alt="not available">
+            </div>
+            <div class="information">
+                <p>Country: ${element.name.common}</p>
+                <p>Population: ${element.population}</p>
+                <p>Currency: ${currencyName}</p>
+                <p>Symbol: ${currencySymbol}</p>
+            </div>
+        </div>`
+            
+            
+        })
+    })
+}
+
 searchBox.addEventListener("keydown", searched)
 searchBtn.addEventListener("click", searched)
-
-
+regionBar.addEventListener("change", region)
 
